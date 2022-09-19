d8ca29c4c5b3535659cba961cce28d87cea8cd3c
Eunbi <joeunbi514@naver.com>
2022-09-19 21:22:04 +0900

    1012.cpp

--- algorithm/0x09 BFS/1012.cpp
+++ algorithm/0x09 BFS/1012.cpp
@@ -13,40 +13,41 @@ int main(void) {
 
     cin >> t;
     while(t--) {
+        for(int i=0; i<51; i++) {
+            for(int j=0; j<51; j++) {
+                board[i][j] = 0;
+                vis[i][j] = 0;
+            }
+        }
         cin >> m >> n >> k;
         int x=0,y=0;
         while(k--) {
             cin >> x >> y;
             board[y][x] = 1;
         }
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                cout << board[i][j];
-            }
-            cout << '\n';
-        }
         
         queue<pair<int,int>> Q;
         int sum = 0;
         for(int i=0; i<n; i++) {
             for(int j=0;j<m;j++) {
-                if(board[i][j]==1 && vis[i][j]==0) Q.push({i,j});
+                if(board[i][j]==1 && vis[i][j]==0) {
+                    sum++;
+                    Q.push({i,j});
+                } 
                 while(!Q.empty()) {
                     pair<int,int> cur = Q.front(); Q.pop();
                     for(int dir=0; dir<4; dir++) {
                         int nx = cur.first + dx[dir];
                         int ny = cur.second + dy[dir];
-                        if(nx <0 || nx >= m || ny <0 || ny >= n) continue;
+                        if(nx <0 || nx >= n || ny <0 || ny >= m) continue;
                         if(board[nx][ny] != 1 || vis[nx][ny]) continue;
                         vis[nx][ny]=1;
                         Q.push({nx,ny});
                     }
                 }
-                sum++;
             }
         }
-
-        cout << sum;
+        cout << sum << '\n';
     }
 
     
