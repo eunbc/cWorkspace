0130dcf01fb2678966fd66a58de2cbb39645ed9a
ebcho <joeunbi514@naver.com>
2022-11-24 14:08:44 +0900

    1074.cpp Z

---
+++ algorithm/0x0B recursive/1074.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int n, int r, int c) {
+    if(n==0) return 0;
+    int half = 1<<n;
+    half /= 2;
+    //1번 사각형
+    if(r < half && c < half) {
+        return func(n-1, r, c);
+    } 
+    //2번 사각형
+    else if(r < half && c >= half) {
+        return half*half + func(n-1, r, c-half);
+    }
+    //3번 사각형
+    else if(r >= half && c < half) {
+        return 2*half*half + func(n-1, r-half, c);
+    }
+    //4번 사각형
+    else {
+        return 3*half*half + func(n-1, r-half, c-half);
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,r,c;
+    cin >> n >> r >> c;
+    cout << func(n,r,c);
+}
