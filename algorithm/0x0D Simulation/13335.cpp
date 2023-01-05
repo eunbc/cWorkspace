3175beda89e507b13c4d012c6e311db6148d40eb
ebcho <joeunbi514@naver.com>
2023-01-05 13:04:55 +0900

    시뮬레이션 트럭, 드래래곤 커브

--- algorithm/0x0D Simulation/13335.cpp
+++ algorithm/0x0D Simulation/13335.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,w,L,ans;
+int n, w, L, ans;
 int bridge[100];
 queue<int> truck;
+
+bool isEmpty() {
+    for(int i=0; i<w; i++) {
+        if(bridge[i]) return false;
+    }
+    return true;
+}
 
 int calculate() {
     int sum = 0;
@@ -13,15 +20,8 @@ int calculate() {
     return sum;
 }
 
-bool isEmpty(){
-    for(int i=0; i<w; i++) {
-        if(bridge[i]) return false;
-    }
-    return true;
-}
-
 void go() {
-    for(int i=w-1; i>0 ; i--) {
+    for(int i=w-1; i>0; i--) {
         bridge[i] = bridge[i-1];
     }
     bridge[0] = 0;
@@ -30,18 +30,19 @@ void go() {
 int main(void){ 
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> n >> w >> L;
+
+    cin >> n >> w >> L ;
     for(int i=0; i<n; i++) {
         int t;
         cin >> t;
         truck.push(t);
     }
-    
+
     do{
-        int tmp = calculate(); 
+        int tmp = calculate();
         tmp -= bridge[w-1];
         go();
-        if(!truck.empty() && (tmp+truck.front()) <= L ) {
+        if(!truck.empty() && tmp + truck.front() <= L) {
             bridge[0] = truck.front(); truck.pop();
         }
         ans++;
--- algorithm/0x0D Simulation/15685.cpp
+++ algorithm/0x0D Simulation/15685.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int N;
 bool board[101][101];
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,-1,0,1};
 
 int main(void){ 
     ios::sync_with_stdio(0);
@@ -11,22 +13,31 @@ int main(void){
     while(N--) {
         int x,y,d,g;
         cin >> x >> y >> d >> g;
+        vector<int> v = {};
+        v.push_back(d%4);
         board[y][x] = 1;
 
         while(g--) {
-            
+            for(int i=v.size()-1; i>=0; i--) {
+                v.push_back((v[i]+1)%4);
+            }
         }
+        
+        for(int i=0; i<v.size(); i++) {
+            int dir = v[i];
+            x += dx[dir];
+            y += dy[dir];
+            board[y][x] = 1;
+        }
+
     }
 
-
+    int ans = 0;
     for(int i=0; i<100; i++) {
-        for(int j=0; j<100 ;j++) {
+        for(int j=0; j<100; j++) {
             if(!board[i][j]) continue;
-            
+            if(board[i+1][j] && board[i][j+1] && board[i+1][j+1]) ans++;
         }
     }
-
-    
-
-
+    cout << ans;
 }
