7c85be33b6122598e615173747846af9843ee07f
ebcho <joeunbi514@naver.com>
2023-01-12 23:35:11 +0900

    dp

---
+++ algorithm/0x10 DP/1932.cpp
@@ -0,0 +1,14 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int n;
+// int tri[][];
+
+// int main(void){ 
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     cin >> n;
+//     for(int i=0; i<n; i++) {
+//         cin >> 
+//     }
+// }
