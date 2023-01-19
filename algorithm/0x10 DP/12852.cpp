cf6824753949a70ed08b867000b1c2da81387a1b
ebcho <joeunbi514@naver.com>
2023-01-19 14:32:40 +0900

    dp

--- algorithm/0x10 DP/12852.cpp
+++ algorithm/0x10 DP/12852.cpp
@@ -3,31 +3,33 @@ using namespace std;
 
 int n;
 int d[1000005];
-vector<int> v;
+int pre[1000005];
 
 int main(void){ 
     ios::sync_with_stdio(0);
     cin.tie(0);
     
     cin >> n;
-    
     d[1] = 0;
-    // v.push_back(1);
     for(int i=2; i<=n; i++) {
         d[i] = d[i-1] + 1;
-        if(i%2==0 && d[i]>(d[i/2]+1)) {
-            d[i] = d[i/2]+1;
-            v.push_back(i/2);
-        } else if(i%3==0 && d[i]>(d[i/3]+1)) {
-            d[i] = d[i/3]+1;
-            v.push_back(i/3);
-        } else {
-            v.push_back(i-1);
+        pre[i] = i-1;
+        if(i%2==0 && min(d[i/2] + 1, d[i]) != d[i]) {
+            d[i] = min(d[i/2] + 1, d[i]);
+            pre[i] = i/2;
+        }
+        if(i%3==0 && min(d[i/3] + 1, d[i]) != d[i]) {
+            d[i] = min(d[i/3] + 1, d[i]);
+            pre[i] = i/3;
         }
     }
 
-    cout << d[n] << '\n';
-    for(int i=0; i<v.size(); i++) {
-        cout << v[i] << ' ';
+    cout << d[n] << '\n';   
+
+    int cur = n;
+    while(true) {
+        cout << cur << ' ';
+        if(cur==1) break;
+        cur = pre[cur];
     }
 }
