81e2f768711f71925e0d4545f01ad153ebca4268
ebcho <joeunbi514@naver.com>
2023-01-20 23:39:27 +0900

    프로그래머스 0120 3문제

--- algorithm/programmers/1.cpp
+++ algorithm/programmers/1.cpp
@@ -1,9 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string solution(string str) {
+//최대값과 최솟값
+//https://school.programmers.co.kr/learn/courses/30/lessons/12939
+
+string solution(string s) {
     string answer = "";
-    
+    istringstream ss(s);
+    string stringBuffer;
+    vector<string> x;
+    x.clear();
+    while (getline(ss, stringBuffer, ' ')){
+        x.push_back(stringBuffer);
+    }
+
+    vector<int> v;
+    for(int i=0; i<x.size(); i++) {
+        v.push_back(stoi(x[i]));
+    }
+
+    int mn = *min_element(v.begin(), v.end());
+    int mx = *max_element(v.begin(), v.end());
+    answer = to_string(mn) + ' ' + to_string(mx);
     return answer;
 }
 
--- algorithm/programmers/2.cpp
+++ algorithm/programmers/2.cpp
@@ -1,17 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//JadenCase 문자열 만들기
+//https://school.programmers.co.kr/learn/courses/30/lessons/12951
+
 string solution(string s) {
-    for(int i=0; i<s.size(); i++) {
-        cout << s[i] << ' ';
-    }
     string answer = "";
+    bool isFirst = true;
+    for(int i=0; i<s.size(); i++) {
+        if(isFirst) {
+            s[i] = toupper(s[i]);
+            isFirst = false;
+        } else if(s[i]==' ' && s[i+1]!=' ') {
+            isFirst = true;
+        } else {
+            s[i] = tolower(s[i]);
+        }
+    }
+    answer = s;
     return answer;
 }
 
 int main(void){ 
     ios::sync_with_stdio(0);
     cin.tie(0);
-    string str = "3people unFollowed me";
+    string str = "for the  last week";
     cout << solution(str);    
 }
