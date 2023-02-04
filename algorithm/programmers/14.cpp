05597abf803d837965138f08c3f9f80a49da4d09
ebcho <joeunbi514@naver.com>
2023-02-04 12:27:26 +0900

    230204 카펫(완전탐색)

---
+++ algorithm/programmers/14.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//N개의 최소공배수
+//https://school.programmers.co.kr/learn/courses/30/lessons/12953
+
+int solution(vector<int> arr) {
+    int answer = 0;
+    return answer;
+}
+
+int main(void){ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int> arr = {2,6,8,14};
+    cout << solution(arr);
+}
