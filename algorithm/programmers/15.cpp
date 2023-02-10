1ac1e2ca2d9a19f92dae8a7dfdf473b342e6ad48
ebcho <joeunbi514@naver.com>
2023-02-10 13:33:02 +0900

    230210 프로그래머스

--- algorithm/programmers/15.cpp
+++ algorithm/programmers/15.cpp
@@ -4,15 +4,21 @@ using namespace std;
 //예상 대진표
 //https://school.programmers.co.kr/learn/courses/30/lessons/12985
 
+int answer = 1;
+
 int solution(int n, int a, int b)
 {
-    int answer = 0;
-
+    a = (a+1)/2;
+    b = (b+1)/2;
+    if(a==b) return answer;
+    answer++;
+    solution(n/2, a, b);
     return answer;
 }
 
 int main(void){ 
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+    int n = 8, a = 4, b = 7;
+    cout << solution(n,a,b);
 }
