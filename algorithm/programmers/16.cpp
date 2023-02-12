ff4a14997bb323de129d16628144eef6171e45a1
ebcho <joeunbi514@naver.com>
2023-02-12 23:51:04 +0900

    230212 프로그래머스

---
+++ algorithm/programmers/16.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dist[1000000005];
+
+//점프와 순간 이동
+//https://school.programmers.co.kr/learn/courses/30/lessons/12980
+
+int solution(int n)
+{
+    int ans = 0;
+    
+    return ans;
+}
+
+int main(void){ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n = 5;
+    cout << solution(n);
+}
