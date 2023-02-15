b137d7ddb5c507cf83249efe6b9264943b841d39
ebcho <joeunbi514@naver.com>
2023-02-15 23:30:20 +0900

    230215 프로그래머스

---
+++ algorithm/programmers/21.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//행렬의 곱셈
+//https://school.programmers.co.kr/learn/courses/30/lessons/12949
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    for(int i=0; i<arr1.size(); i++) {
+        vector<int> v;
+        for(int j=0; j<arr2.size(); j++) {
+            int tmp = 0;
+            for(int k=0; k<arr1[i].size(); k++) {
+                tmp += arr1[i][k] * arr2[k][j];
+            }
+            v.push_back(tmp);
+        }
+        answer.push_back(v);
+    }
+    return answer;
+}
+
+int main(void){ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vector<vector<int>> arr1 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}}; 
+    vector<vector<int>> arr2 = {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
+    vector<vector<int>> res = solution(arr1, arr2);
+    for(int i=0; i<res.size(); i++) {
+        for(int j=0; j<res[i].size(); j++) {
+            cout << res[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
---
+++ algorithm/programmers/22.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//위장
+//https://school.programmers.co.kr/learn/courses/30/lessons/42578
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 0;
+
+    return answer;
+}
+
+int main(void){ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vector<vector<string>> clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+    cout << solution(clothes);    
+}
