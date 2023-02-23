5ccb1fb173a1ee3f36e6caa5380c06bcb4019e9a
ebcho <joeunbi514@naver.com>
2023-02-23 23:12:03 +0900

    230223 이중우선순위 큐

--- algorithm/programmers/28.cpp
+++ algorithm/programmers/28.cpp
@@ -4,34 +4,50 @@ using namespace std;
 //이중우선순위큐
 //https://school.programmers.co.kr/learn/courses/30/lessons/42628
 
-int getNum(string s) {
-    return 0;
-}
-
 vector<int> solution(vector<string> operations) {
-    vector<int> answer;
-    vector<int> q;
+    vector<int> answer(2);
+    priority_queue<int> pq1; //내림차순
+    priority_queue<int,vector<int>, greater<int>> pq2; //오름차순
 
+    int cnt = 0;
     for(int i=0; i<operations.size(); i++) {
+        //Insert
         if(operations[i][0]=='I') {
-            int num = getNum(operations[i]);
-            q.push_back(num);
-        } else if(operations[i]=="D 1") {
-            cout << "D 1" << '\n';
-        } else if(operations[i]=="D -1") {
-            cout << "D -1" << '\n';
+            int num = stoi(operations[i].substr(2));
+            pq1.push(num);
+            pq2.push(num);
+            cnt++;        
         }
-    }
 
-    if(q.empty()) {answer.push_back(0); answer.push_back(0);}
-    else {answer.push_back(*max_element(q.begin(),q.end())); answer.push_back(*min_element(answer.begin(),answer.end())); }
+        //최소값 삭제
+        else if(cnt!=0 && operations[i]=="D -1") {
+            pq2.pop();
+            cnt--;
+        }
+
+        //최대값 삭제
+        else if(cnt!=0 && operations[i]=="D 1") {
+            pq1.pop();
+            cnt--;
+        }
+
+        if(cnt==0) {
+            while(!pq1.empty()) pq1.pop();
+            while(!pq2.empty()) pq2.pop();
+        }
+
+    }
+    if(cnt) {
+        answer[0] = pq1.top();
+        answer[1] = pq2.top();
+    }
     return answer;
 }
 
 int main(void){ 
     ios::sync_with_stdio(0);
     cin.tie(0);
-    vector<string> operations = {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
+    vector<string> operations = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
     vector<int> res = solution(operations);   
     for(int i=0; i<res.size(); i++) {
         cout << res[i] << ' ';
