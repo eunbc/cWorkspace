5172c442c29f6fd00b897570d915b676f61bd7bb
ebcho <joeunbi514@naver.com>
2023-02-26 23:33:00 +0900

    230226 프로그래머스 전화번호 목록, 뉴스 클러스터링

---
+++ algorithm/programmers/31.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//전화번호 목록
+//https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=cpp
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    for(int i=0; i<phone_book.size(); i++) {
+        for(int j=0; j<phone_book.size(); j++) {
+            
+        }
+    }
+    return answer;
+}
+
+int main(void){ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vector<string> phone_book = {"119", "97674223", "1195524421"};
+    cout << solution(phone_book);
+}
